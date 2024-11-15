e2fbc2cfdf04123b9ec9bc528cbc408681eaf89a
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-15 15:39:38 +0100

    feat: almost done

--- js-jsnacks-blocco-5/snack-7/script.js
+++ js-jsnacks-blocco-5/snack-7/script.js
@@ -1,23 +1,33 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const oggettoTrovato = students.find((curElement, curIndex) => {
+  // La parola class è riservata in Javascript quindi non possiamo creare una variabile che si chiama class
+  // Quindi dobbiamo assegnare un alias (studentClass) alla chiave
+  const { id, name, age, class: studentClass } = curElement;
+  // console.log(studentClass);
+  // Ritorniamo se id è uguale a 2
+  return id === 2;
+});
+console.log(oggettoTrovato);
