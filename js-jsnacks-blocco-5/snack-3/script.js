d3a09aa47dc68f39bffbb2512890c8faf7b697b7
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-15 15:12:15 +0100

    feat: snack 3-4 done

--- js-jsnacks-blocco-5/snack-3/script.js
+++ js-jsnacks-blocco-5/snack-3/script.js
@@ -2,3 +2,16 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// Il metodo map crea un nuovo array dove ad ogni elemento dell'array viene eseguita una funzione di callback
+// Questa funzione deve avere un ritorno che sarà il nuovo elemento
+// Il nuovo array creato avrà lo stesso numeri di elementi dell'array dove è applicato il map
+
+const newArray = numbers.map((curElement, curIndex) => {
+  const newElement = curElement + 1;
+  return newElement;
+});
+
+console.log(newArray);
+
+console.log(numbers);
--- js-jsnacks-blocco-5/snack-4/script.js
+++ js-jsnacks-blocco-5/snack-4/script.js
@@ -1,9 +1,23 @@
 const posts = [
-  { author: 'Marco', date: '12/3/2021', content: 'lorem ipsum...' },
-  { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
-  { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
-  { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+  { author: "Marco", date: "12/3/2021", content: "lorem ipsum..." },
+  { author: "Luca", date: "30/6/2021", content: "lorem ipsum..." },
+  { author: "Fabrizio", date: "12/9/2020", content: "lorem ipsum..." },
+  { author: "Enrico", date: "09/12/2021", content: "lorem ipsum..." },
+];
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+// Nuovo array globale per contenere la proprietà author di ogni oggetto nell'array precedente
+const newArray = [];
+
+//
+posts.forEach((curElement, curIndex) => {
+  // Destructuring oggetto
+  const { author, date, content } = curElement;
+  // Pushamo la proprietà con chiave author
+  // Nel foreach il push non è implicito
+  newArray.push(author);
+});
+
+console.log(newArray);
