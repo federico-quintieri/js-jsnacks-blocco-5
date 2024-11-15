f2d89e15007d9e5c6865ab2e420f71036b131d95
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-15 15:02:36 +0100

    feat:1-2 exercise done

--- js-jsnacks-blocco-5/snack-1/script.js
+++ js-jsnacks-blocco-5/snack-1/script.js
@@ -1,4 +1,12 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+// Il metodo for each esegue una funzione (callback) per ogni elemento dell'array
+names.forEach((curEleme, curIndex) => {
+  console.log(curEleme);
+});
+
+
