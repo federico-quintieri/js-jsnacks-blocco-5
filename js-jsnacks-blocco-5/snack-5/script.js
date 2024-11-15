73ca4acece0ce887964b99f7a62a59b36cad759d
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-15 15:22:26 +0100

    feat: 5-6 done

--- js-jsnacks-blocco-5/snack-5/script.js
+++ js-jsnacks-blocco-5/snack-5/script.js
@@ -2,3 +2,12 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// Il metodo filter restituisce un nuovo array contenente solo gli elementi dell'array di partenza che hanno soddisfatto la condizione nella callback function
+// L'array risultato può essere meno lungo dell'attuale
+// Il metodo push è implicito in caso di ritorno true della call back functions
+const newArray = nums.filter((curElement, curIndex) => {
+  return curElement % 2 === 0;
+});
+
+console.log(newArray);
--- js-jsnacks-blocco-5/snack-6/script.js
+++ js-jsnacks-blocco-5/snack-6/script.js
@@ -1,14 +1,33 @@
 const zucchine = [
-  { type: 'Napoletana', weight: 10, length: 4 },
-  { type: 'Trombetta', weight: 13, length: 16 },
-  { type: 'Napoletana', weight: 4, length: 23 },
-  { type: 'Trombetta', weight: 11, length: 6 },
-  { type: 'Napoletana', weight: 2, length: 17 },
-  { type: 'Romana', weight: 5, length: 10 },
-  { type: 'Romana', weight: 7, length: 9 },
-  { type: 'Trombetta', weight: 3, length: 8 },
-  { type: 'Calabrese', weight: 6, length: 27 },
-  { type: 'Calabrese', weight: 14, length: 4 },
+  { type: "Napoletana", weight: 10, length: 4 },
+  { type: "Trombetta", weight: 13, length: 16 },
+  { type: "Napoletana", weight: 4, length: 23 },
+  { type: "Trombetta", weight: 11, length: 6 },
+  { type: "Napoletana", weight: 2, length: 17 },
+  { type: "Romana", weight: 5, length: 10 },
+  { type: "Romana", weight: 7, length: 9 },
+  { type: "Trombetta", weight: 3, length: 8 },
+  { type: "Calabrese", weight: 6, length: 27 },
+  { type: "Calabrese", weight: 14, length: 4 },
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+// Creo due array per contenere i nuovi oggetti filtrati
+const array_1 = [];
+const array_2 = [];
+
+// Partendo dall'array di oggetti zucchine esamino ogni elemento eseguendo una call back function
+zucchine.forEach((curElement, curIndex) => {
+  // Prelevo solo la lunghezza delle zucchine dall'oggetto
+  const { length } = curElement;
+  // console.log(length);
+
+  // Se la proprietà length dell'oggetto è maggiore a 15 pusho tutto l'oggetto (curElement) nell'array 1
+  if (length > 15) array_1.push(curElement);
+  // Altrimenti pusho tutto l'oggetto (curElement) nell'array 2
+  else array_2.push(curElement);
+});
+
+console.log(array_1);
+console.log(array_2);
